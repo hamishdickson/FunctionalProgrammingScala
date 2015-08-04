2ce7bfbb79d85d44eb64e4e2adbf6b11ed2a1ada
hamishdickson <hamish.dickson@gmail.com>
2015-08-04 20:37:24 +0100

    first HOF

--- src/main/scala/chapter2/MyModule.scala
+++ src/main/scala/chapter2/MyModule.scala
@@ -13,6 +13,9 @@ object MyModule {
     go(n, 1)
   }
 
+  /**
+   * notice formatAbs and formatFactorial are basically the same
+   */
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
@@ -23,8 +26,18 @@ object MyModule {
     msg.format(n, factorial(n))
   }
 
+  /**
+   * Write a higher order function to take their place
+   */
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(formatFactorial(7))
+
+    println(formatResult("factorial", 7, factorial))
   }
 }
