48620cae71ab3d5059c7f2bfe7d86b024e4d90ee
hamishdickson <hamish.dickson@gmail.com>
2015-08-04 19:48:46 +0100

    add test for factorial

--- src/main/scala/chapter2/MyModule.scala
+++ src/main/scala/chapter2/MyModule.scala
@@ -18,6 +18,13 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit =
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+  }
+
+  def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(formatFactorial(7))
+  }
 }
