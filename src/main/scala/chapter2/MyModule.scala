dc7b751030dbe15d8bacc8dc551da0d899e57854
hamishdickson <hamish.dickson@gmail.com>
2015-08-04 20:50:27 +0100

    polymorphic function

--- src/main/scala/chapter2/MyModule.scala
+++ src/main/scala/chapter2/MyModule.scala
@@ -34,6 +34,38 @@ object MyModule {
     msg.format(name, n, f(n))
   }
 
+  /**
+   * example of a monomorphic function
+   *
+   * gives first index of a key in an array (or -1 if not found)
+   */
+  private def findFirst(ss: Array[String], key: String): Int = {
+    def loop(n: Int): Int =
+      if (n >= ss.length) -1
+      else if (ss(n) == key) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  /**
+   * example of parametric polymorphic function (aka generic function)
+   *
+   * idea here is that the findFirst logic is the same no matter what type of array we are
+   * looking at - so should be able to do this with any type of array
+   *
+   * instead of hard coding a string, take a type A as a parameter and instead of hard coding
+   * equality of the key, take a function to test for equality
+   */
+  private def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n // if p gives true, then found the key
+      else loop(n + 1)
+
+    loop(0)
+  }
+
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(formatFactorial(7))
