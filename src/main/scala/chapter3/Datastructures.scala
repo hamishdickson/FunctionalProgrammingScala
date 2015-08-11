9943ad3b93bffc11959b54c825b0671acfd89cd8
hamishdickson <hamish.dickson@gmail.com>
2015-08-11 20:22:39 +0100

    exercise 3.1

--- src/main/scala/chapter3/Datastructures.scala
+++ src/main/scala/chapter3/Datastructures.scala
@@ -5,5 +5,20 @@ object Datastructures {
     val ex1: List[Double] = Nil
     val ex2: List[Int] = Cons(1, Nil)
     val ex3: List[String] = Cons("a", Cons("b", Nil))
+
+
+    /**
+     * Exercise 3.1: What is the result of the following?
+     */
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + List.sum(t) // not sure why this has to be List.sum
+      case _ => 101
+    }
+
+    println("Val x: " + x)
   }
+
 }
