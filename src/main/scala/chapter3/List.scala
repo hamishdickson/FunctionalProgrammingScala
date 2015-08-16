f59fb0e387b1202256d2476eb82f7f892a044bb9
hamishdickson <hamish.dickson@gmail.com>
2015-08-16 10:16:38 +0100

    exercise 3.4

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -48,4 +48,17 @@ object List {
     case Nil => Nil
     case Cons(_, t) => Cons(v, t)
   }
+
+  /**
+   * Exercise 3.4: Generalize tail to the function drop, which removes the first n elements from a list. Note that this
+   * function takes time proportional only to the number of elements being dropped - we don't need to make a cope of the
+   * entire list
+   */
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n-1)
+    }
+  }
 }
