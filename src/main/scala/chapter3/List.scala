973bc688ae8f38ad3be6405591cd44baa990aec8
hamishdickson <hamish.dickson@gmail.com>
2015-08-16 10:24:22 +0100

    exercise 3.5

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -61,4 +61,12 @@ object List {
       case Cons(_, t) => drop(t, n-1)
     }
   }
+
+  /**
+   * Exercise 3.5: Implement dropWhile, which removes elements from the List prefix as long as they match a predicate
+   */
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t, f)
+    case _ => l
+  }
 }
