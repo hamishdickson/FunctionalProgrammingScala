1c46d691ec68eefbc5266222f205d382f7263f65
hamishdickson <hamish.dickson@gmail.com>
2015-08-29 11:41:13 +0100

    exercise 2.27 and 28

--- src/main/scala/chapter3/Trees.scala
+++ src/main/scala/chapter3/Trees.scala
@@ -1,4 +1,4 @@
-package chaspter3
+package chapter3
 
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
@@ -19,5 +19,21 @@ object Tree {
   def maximum(t: Tree[Int]): Int = t match {
     case Leaf(n) => n
     case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  /**
+   * Exercise 2.27: Write depth, which returns the depth of the deepest leaf
+   */
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  /**
+   * Exercise 2.28: Write a function map, analogus to map in List
+   */
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(n) => Leaf(f(n))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
 }
