f0bec7256987ee9d5ac124f868ba24362ba112cc
hamishdickson <hamish.dickson@gmail.com>
2015-09-14 21:49:07 +0100

    exercise 5.4

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -50,6 +50,12 @@ sealed trait Stream[+A] {
   }
 
   def existsFoldRight(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
+
+  /**
+   * Exercise 5.4: Implement forAll which checks that all elements in the Stream match a given
+   * predicate. The impl should terminate the traversal as soon as it encounters a non-matching value
+   */
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
 }
 
 case object Empty extends Stream[Nothing]
