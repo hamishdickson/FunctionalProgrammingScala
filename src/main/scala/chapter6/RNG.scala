c4e5c2e63de7a13085bac9ed9a3b9f09e52d852e
hamishdickson <hamish.dickson@gmail.com>
2015-10-01 08:46:25 +0100

    create chapter 6

---
+++ src/main/scala/chapter6/RNG.scala
@@ -0,0 +1,14 @@
+package chapter6
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+// a purely functional random number generator
+case class SimpleRNG(seed: Long) extends RNG {
+  override def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
