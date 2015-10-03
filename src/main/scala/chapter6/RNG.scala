95e6ea0a6b41a7ecd7260fe03d927c7b0d435d65
hamishdickson <hamish.dickson@gmail.com>
2015-10-03 18:45:22 +0100

    add random pairs

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -3,12 +3,33 @@ package chapter6
 trait RNG {
   def nextInt: (Int, RNG)
 }
-// a purely functional random number generator
+
+/**
+  * RNG: a purely functional random number generator
+  */
 case class SimpleRNG(seed: Long) extends RNG {
   override def nextInt: (Int, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
     val nextRNG = SimpleRNG(newSeed)
     val n = (newSeed >>> 16).toInt
     (n, nextRNG)
+  }
+
+  /**
+   * returns a pair of the same number
+   */
+  def randomSamePair(rng: RNG): (Int, Int)  = {
+    val (i1, _) = rng.nextInt
+    val (i2, _) = rng.nextInt
+    (i1, i2)
+  }
+
+  /**
+   * returns a pair of random numbers
+   */
+  def randomPair(rng: RNG): ((Int, Int), RNG) = {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng2.nextInt
+    ((i1, i2), rng3)
   }
 }
