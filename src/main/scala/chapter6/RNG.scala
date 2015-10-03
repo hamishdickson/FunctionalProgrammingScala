dd25b514450285e459f0caf9154a942ebc7cd075
hamishdickson <hamish.dickson@gmail.com>
2015-10-03 22:43:40 +0100

    exercise 6.4

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -80,4 +80,16 @@ case class SimpleRNG(seed: Long) extends RNG {
 
     ((d1,d2,d3), rng3)
   }
+
+  /**
+   * Exercise 6.4: Write a function to generate a list of random integers
+   */
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0) (List(), rng)
+    else {
+      val (x, r1)  = rng.nextInt
+      val (xs, r2) = ints(count - 1)(r1)
+      (x :: xs, r2)
+    }
+  }
 }
