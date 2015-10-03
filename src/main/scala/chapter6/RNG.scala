4ff87ba6b043fcf75ab1e35c17c2d5a6fdec47c6
hamishdickson <hamish.dickson@gmail.com>
2015-10-03 21:17:52 +0100

    exercise 6.3

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -55,4 +55,29 @@ case class SimpleRNG(seed: Long) extends RNG {
 
     (i1 / Int.MaxValue.toDouble, rng1)
   }
+
+  /**
+   * Exercise 6.3: Write functions to generate an (Int, Double) pair, a (Double, Int) pair, and a
+   * (Double, Double, Double) 3-tuple. You should be able to reuse the functions you've already written
+   */
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i1, rng1) = rng.nextInt
+    val (d1, rng2) = double(rng1)
+
+    ((i1,d1), rng2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i1, d1), rng1) = intDouble(rng)
+
+    ((d1,i1), rng1)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+
+    ((d1,d2,d3), rng3)
+  }
 }
