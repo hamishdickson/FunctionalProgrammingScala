6d5978c5257ba867b87a228d124c13e2a6d60e3f
hamishdickson <hamish.dickson@gmail.com>
2015-10-10 11:22:01 +0100

    exercise 6.10

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -186,3 +186,36 @@ case class SimpleRNG(seed: Long) extends RNG {
   def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] =
     flatMap(ra)(a => map(rb)(b => f(a, b)))
 }
+
+/**
+ * Exercise 6.10: Generalise the functions unit, map, map2, flatMap and sequence
+ */
+import State._
+
+case class State[S, +A](run: S => (A,S)) {
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => unit(f(a)))
+
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a,b)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  def unit[S,A](a: A): State[S,A] = State(s => (a,s))
+
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s: S, actions: List[State[S,A]], acc: List[A]): (List[A],S) =
+      actions match {
+        case Nil => (acc.reverse,s)
+        case h :: t => h.run(s) match { case (a,s2) => go(s2, t, a :: acc) }
+      }
+    State((s: S) => go(s,sas,List()))
+  }
+}
