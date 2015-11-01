6525e52c8c6bc4d098b2487a4bca2be9fb9417ff
hamishdickson <hamish.dickson@gmail.com>
2015-11-01 17:05:17 +0000

    minor change

--- src/main/scala/chapter7/Nonblocking.scala
+++ src/main/scala/chapter7/Nonblocking.scala
@@ -1,13 +1,12 @@
 package chapter7
 
-import java.util.concurrent.{Callable, CountDownLatch, ExecutorService}
+import java.util.concurrent._
 
 object Nonblocking {
   sealed trait Future[A] {
     private[Parallelism] def apply(k: A => Unit): Unit
   }
 
-  // this looks the same, but with a non-blocking future
   type Par[+A] = ExecutorService => Future[A]
 
   object Par {
