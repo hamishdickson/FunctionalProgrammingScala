6c272ea393323e51bdd26c980045bd401d1b65e6
hamishdickson <hamish.dickson@gmail.com>
2015-12-09 08:29:29 +0000

    more c8

--- src/main/scala/chapter8/Gen.scala
+++ src/main/scala/chapter8/Gen.scala
@@ -1,5 +1,19 @@
 package chapter8
 
-class Gen {
+trait Prop {
+  def check: Unit
+  def &&(p: Prop): Prop = ???
+}
 
+
+object Gen {
+  /**
+    * Generates a list of type A
+    */
+  def listOf[A](a: Gen[A]): Gen[List[A]]
+
+  /**
+    * Creates n elements in a listOf
+    */
+  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]]
 }
