2af92f32a4ff2fd6fb3fc5bb834ec076b56c4038
hamishdickson <hamish.dickson@gmail.com>
2015-12-21 20:00:37 +0000

    test cases for listOfN

--- src/main/scala/chapter9/Parsers.scala
+++ src/main/scala/chapter9/Parsers.scala
@@ -55,6 +55,11 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(implicit  f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
 
+  /**
+    * run(listOfN(3, "ab" | "cad"))("ababcad") == Right("ababcad")
+    * run(listOfN(3, "ab" | "cad"))("cadabab") == Right("cadabab")
+    * run(listOfN(3, "ab" | "cad"))("ababab") == Right("ababab")
+    */
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
 
   case class ParserOps[A](p: Parser[A]) {
