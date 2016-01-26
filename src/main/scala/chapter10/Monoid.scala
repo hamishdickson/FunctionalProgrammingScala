92b75eb80c875143bfee8e36f6daf5ade5a47e95
hamishdickson <hamish.dickson@gmail.com>
2016-01-26 18:57:23 +0000

    monoid examples

--- src/main/scala/chapter10/Monoid.scala
+++ src/main/scala/chapter10/Monoid.scala
@@ -6,9 +6,33 @@ package chapter10
   * - an associative binary operation `op`, that takes values of type A and combines them into another
   *       op(op(x,y),z) == op(x, op(y,z)) ∀ x,y,z in A
   * - a value `zero` that is the identity for `op`, ie op(x, zero) == x and op(zero, x) == x
+  *
+  *
+  * Useful to know:
+  * - means you can parallelise problem
+  * - can be composed to assemble complex calculations from simpler pieces
   */
 
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
+}
+
+object Monoid {
+  /**
+    * Example - string monoid
+    */
+  val stringMonoid = new Monoid[String] {
+    override def op(a1: String, a2: String): String = a1 + a2
+    override def zero: String = ""
+  }
+
+  /**
+    * List concatenation also forms a monoid
+    */
+  def listMonoid[A] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
+    override def zero: List[A] = Nil
+  }
 }
