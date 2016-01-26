29d6715e3b4868f2c2fac996b5331b7e9f922311
hamishdickson <hamish.dickson@gmail.com>
2016-01-26 08:20:17 +0000

    start monoids

---
+++ src/main/scala/chapter10/Monoid.scala
@@ -0,0 +1,14 @@
+package chapter10
+
+/**
+  * A monoid consists of the following:
+  * - some type A
+  * - an associative binary operation `op`, that takes values of type A and combines them into another
+  *       op(op(x,y),z) == op(x, op(y,z)) ∀ x,y,z in A
+  * - a value `zero` that is the identity for `op`, ie op(x, zero) == x and op(zero, x) == x
+  */
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
