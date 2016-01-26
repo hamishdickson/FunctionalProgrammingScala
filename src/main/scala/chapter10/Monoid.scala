97c0ea3063d34b107854904bd185d5e09d1d0aec
hamishdickson <hamish.dickson@gmail.com>
2016-01-26 19:01:57 +0000

    monoid 10.1

--- src/main/scala/chapter10/Monoid.scala
+++ src/main/scala/chapter10/Monoid.scala
@@ -35,4 +35,31 @@ object Monoid {
 
     override def zero: List[A] = Nil
   }
+
+  /**
+    * Exercise 10.1: Give instances for the below
+    */
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+
+    override def zero: Int = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+
+    override def zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override def zero: Boolean = true
+  }
 }
