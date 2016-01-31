bb117e1cf2f8cee82743f3a242749afa1fd006f4
hamishdickson <hamish.dickson@gmail.com>
2016-01-31 18:39:36 +0000

    exercise 10.14

--- src/main/scala/chapter10/Monoid.scala
+++ src/main/scala/chapter10/Monoid.scala
@@ -292,3 +292,23 @@ object TreeFoldable extends Foldable[Tree] {
     case Branch(l, r) => mb.op(foldMap(l)(f)(mb), foldMap(r)(f)(mb))
   }
 }
+
+/**
+  * Exercise 10.14: Implement foldable for option
+  */
+object OptionFoldable extends Foldable[Option] {
+  override def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B): B = as match {
+    case None => z
+    case Some(a) => f(a, z)
+  }
+
+  override def foldLeft[A, B](as: Option[A])(z: B)(f: (B, A) => B): B = as match {
+    case None => z
+    case Some(a) => f(z, a)
+  }
+
+  override def foldMap[A, B](as: Option[A])(f: (A) => B)(mb: Monoid[B]): B = as match {
+    case None => mb.zero
+    case Some(a) => f(a)
+  }
+}
