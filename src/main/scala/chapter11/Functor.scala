1784cbde357aa3b5d2233aecc6df9fe192d99e1d
hamishdickson <hamish.dickson@gmail.com>
2016-02-04 20:07:05 +0000

    Functorssssss

--- src/main/scala/chapter11/Functor.scala
+++ src/main/scala/chapter11/Functor.scala
@@ -8,9 +8,27 @@ import scala.language.higherKinds
   */
 trait Functor[F[_]] {
   def map[A,B](fa: F[A])(f: A => B): F[B]
+
+  /**
+    * Distribute, this is sometime also called `unzip`
+    */
+  def distribute[A,B](fab: F[(A,B)]): (F[A], F[B]) = (map(fab)(_._1), map(fab)(_._2))
+
+  /**
+    * When we have an operation on a product like this, we should see if we can construct the opposite operation over a
+    * sum or coproduct
+    */
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A,B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fb) => map(fb)(Right(_))
+  }
 }
 
 object Functor {
+  /**
+    * we say that a type constructor like list or option or F is a functor and the Functor[F] instance constitues
+    * proof that F is in fact a functor
+    */
   val listFunction = new Functor[List] {
     override def map[A, B](fa: List[A])(f: (A) => B): List[B] = fa map f
   }
