84d64db48c4184be327c7f8c302255a760d3c374
hamishdickson <hamish.dickson@gmail.com>
2016-02-11 08:39:38 +0000

    kelisi compose

--- src/main/scala/chapter11/Monad.scala
+++ src/main/scala/chapter11/Monad.scala
@@ -52,6 +52,14 @@ trait Monad[F[_]] extends Functor[F] {
 
   def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] =
     ms.foldRight(unit(List[A]()))((x,y) => compose(f, (b: Boolean) => if (b) map2(unit(x),y)(_ :: _) else y)(x))
+
+
+  /**
+    * Kleisli
+    *
+    * Exercise 11.7: Implement the kleisli composition function compose
+    */
+  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] = a => flatMap(f(a))(g)
 }
 
 object Monad {
