fb205c4c4c3c96e85aca5a6686c276908e6eb76f
hamishdickson <hamish.dickson@gmail.com>
2016-02-17 19:14:14 +0000

    exercise 12.1

--- src/main/scala/chapter12/Applicative.scala
+++ src/main/scala/chapter12/Applicative.scala
@@ -2,6 +2,8 @@ package chapter12
 
 import chapter11.Functor
 
+import scala.language.higherKinds
+
 /**
   * The applicative trait is less powerful than the monad one, but that comes with it's own benefits
   *
@@ -18,4 +20,13 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a,b) => map2(f(a), b)(_ :: _))
+
+  /**
+    * Exercise 12.1: Trasplant the implementations of as many combinators as you can from Monad to Applicative,
+    * using only map2 and unit or methods implemented in terms of them
+    */
+  //lma.foldRight(unit(List[A]()))((h,t) => map2(h, t)(_ :: _))
+  def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
+  def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] = map2(fa, fb)((_, _))
 }
