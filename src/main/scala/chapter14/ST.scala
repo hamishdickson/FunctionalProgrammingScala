1a2939325b8eee25c6fca8bfe94dcc1965839648
hamishdickson <hamish.dickson@gmail.com>
2016-06-29 08:24:09 +0100

    ST

---
+++ src/main/scala/chapter14/ST.scala
@@ -0,0 +1,30 @@
+package chapter14.st
+
+/**
+  * local effects monad ST (think state transition)
+  */
+sealed trait ST[S,A] { self =>
+  protected def run(s: S): (A,S)
+  def map[B](f: A => B): ST[S,B] = new ST[S,B] {
+    def run(s: S) = {
+      val (a, s1) = self.run(s)
+      (f(a), s1)
+    }
+  }
+
+  def flatMap[B](f: A => ST[S,B]): ST[S,B] = new ST[S,B] {
+    def run(s: S) = {
+      val (a, s1) = self.run(s)
+      f(a).run(s1)
+    }
+  }
+}
+
+object ST {
+  def apply[S,A](a: => A) = {
+    lazy val memo = a
+    new ST[S,A] {
+      def run(s: S) = (memo, s)
+    }
+  }
+}
