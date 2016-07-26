e4633ece6fcffa046b50e268547e1e0f32d61c47
hamishdickson <hamish.dickson@gmail.com>
2016-07-26 20:47:32 +0100

    update

--- src/main/scala/chapter14/ST.scala
+++ src/main/scala/chapter14/ST.scala
@@ -28,3 +28,39 @@ object ST {
     }
   }
 }
+
+/**
+  * methods to read/write on the cell are pure since they just return ST Actions. Note that the type of
+  * ST isn't the type of the cell that's being mutated
+  *
+  * note STRef is sealed and the only way to
+  */
+sealed trait STRef[S,A] {
+  protected var cell: A
+  def read: ST[S, A] = ST(cell)
+  def write(a: A): ST[S, Unit] = new ST[S, Unit] {
+    def run(s: S) = {
+      cell = a
+      ((), s)
+    }
+  }
+}
+
+object STRef {
+  def apply[S,A](a: A): ST[S, STRef[S,A]] = ST(new STRef[S,A] {
+    var cell = a
+  })
+}
+
+object Example {
+  for {
+    r1 <- STRef[Nothing, Int](1)
+    r2 <- STRef[Nothing, Int](1)
+    x  <- r1.read
+    y  <- r2.read
+    _  <- r1.write(y + 1)
+    _  <- r2.write(x + 1)
+    a  <- r1.read
+    b  <- r2.read
+  } yield(a, b)
+}
